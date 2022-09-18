7e1a5dfa53c63bc6e8544a920961204b3461cc7d
cmm-7 <carlosmendoza@pursuit.org>
2022-09-18 08:47:33 -0400

    9-2-front-end-project

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# 9-2-front-end-project
+# 9-2-Front-end Portfolio Project: Soccer App
+
+## Set up the Project
+This application will consume an API which returns dates, times, and scores of professional soccer team matches. Cards with information on a specific soccer match will be displayed to the user. Users will be able to click on League competitions to filter the matches that falls into the competition and set a date range to filter matches within the selected range.
+
+
+### Key Features
+Your application will be completed once it has the following features:
+- [ ] As a user, I can see that once the page loads there are matches of today's date so as soon as I enter the website I can see what is happening today. 
+- [ ] As a user, I can select a date range so that I can see the matches within the given range from what ever league.
+- [ ] As a user, I can see a list of leagues that I can click so I can see only soccer matches within that given league.
+- [ ] As a user, I can see display cards so that I can view the teams playing and the time, date, and status of the game (i.e. "FINISHED").
+- [ ] As a user, I can hover over the display card so I can see the score or get updated on when the match will occur.
+
+
+## Instructions
+Following the instructions below will get you started on creating the Soccer application. These instructions will not serve as a tutorial. You will need to fill in the gaps with your knowledge and research.
+
+When first starting the project, focus on functionality over style. Your goal should be to get the key features of the application working as quickly as possible before you spend a lot of time improving the styling of the application.
+
+### Set up the project 
+Before doing anything, you should complete the following steps.
+
+* Create a new GitHub repository. Create a local Git repository. Create a readme.md file within the local repository. Add and commit your changes, and then connect the local and remote repositories.
+
+* Create the following files within your local repository:
+
+    * about.html
+    * index.html
+    * main.js
+    * style.css
+
+ * Add the basic boilerplate HTML template to your index.html file. Add some text to the page and then open your index.html file to ensure that everything is working.
+
+ * Connect the style.css file to your index.html page. Add a single CSS rule to your style.css file to ensure that everything is connected correctly.
+
+ * Follow a similar process for your main.js file. Make sure that your JavaScript runs after the page has fully been loaded.
+
+*  Make a new commit with all of your changes. Then, push up your code to GitHub.
+
+ * Deploy your application. While it's not much, making sure that you can deploy now will save you time in the future.
+
+### Header
+Create a header at the top of your page. Your header should include the name of the application, link to your About HTML file, and two inputs to submit two dates. 
+
+### API Familiarity
+Use the shared API to access the soccer matches data. Before working with JavaScript, make sure you can access the API through your browser or through another means.
+
+Try console logging information from the API to the console on the DOM to be sure that you can access it well. 
+
+The end points you can choose to practice and use for this project is: 
+* competitions
+* teams
+* matches
+
+### Main Section Structure
+Create the list of leagues below the input submit. Use the leagues that you have access to so that you can make API requests for match information later. The leagues should be lined up and adjust according to the window size. The rest of the page should be blank. At this time, you can choose to begin adding some color.
+
+### Match Cards
+Begin creating the match cards to display match information. Use this link as a reference to creating the cards: https://codepen.io/luanmanara/pen/poyLbWx. The information that we want to display to the DOM is the home and away team name, team crest, match date, match time, and match status. Depending on the status, you want to display certain information for when a user hovers over the card: 
+* If match status is "FINISHED" or "IN_PLAY", then display the soccer of both teams
+* If match status is "POSTPONED", then display "TBD"
+* If match status is "SCHEDULED", then display the date of the match
+
+Use grid to structure the information on the card by seperating them into columns.
+
+Here you will be implementing the API to display the match data.
+
--- main.js
+++ main.js
@@ -7,11 +7,209 @@ const options = {
     // mode: "no-cors",
 }
 
-//fetch("https://api.football-data.org/v4/persons/2019/matches?status=FINISHED",options)
-fetch("http://api.football-data.org/v2/teams/", options)
-.then((response) => response.json())
-.then((json) => {
-    console.log(json)
+getApi();
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const startDate = document.querySelector("input#start");
+    const endDate = document.querySelector("input#end");
+
+
+    console.log(startDate.value);
+    getApi("/",startDate.value, endDate.value);
+
 })
-.catch(console.log)
+
+const aside = document.querySelectorAll("section ul");
+for(let league of aside){
+    league.addEventListener("click", (event) => {
+        event.preventDefault();
+        let leagueClicked = ""
+        //console.log(league.textContent);
+        switch(league.textContent){
+            case "FIFA World Cup":
+            leagueClicked = "/competitions/WC/"
+            //console.log(leagueClicked);
+            getApi(leagueClicked);
+            break;
+            case "UEFA Champions League":
+            leagueClicked = "/competitions/CL/"
+            getApi(leagueClicked);
+            break;
+            case "Bundesliga":
+            leagueClicked = "/competitions/BL1/"
+            getApi(leagueClicked);
+            break;
+            case "Eredivisie":
+            leagueClicked = "/competitions/DED/"
+            getApi(leagueClicked);
+            break;
+            case "Campeonato Brasileiro Série A":
+            leagueClicked = "/competitions/BSA/"
+            getApi(leagueClicked);
+            break;
+            case "Primera Division":
+            leagueClicked = "/competitions/PD/"
+            getApi(leagueClicked);
+            break;
+            case "Ligue 1":
+            leagueClicked = "/competitions/FL1/"
+            getApi(leagueClicked);
+            break;
+            case "Championship":
+            leagueClicked = "/competitions/ELC/"
+            getApi(leagueClicked);
+            break;
+            case "Primeira Liga":
+            leagueClicked = "/competitions/PPL/"
+            getApi(leagueClicked);
+            break;
+            case "European Championship":
+            leagueClicked = "/competitions/EC/"
+            getApi(leagueClicked);
+            break;
+            case "Serie A":
+            leagueClicked = "/competitions/SA/"
+            getApi(leagueClicked);
+            break;
+            case "Premier League":
+            leagueClicked = "/competitions/PL/"
+            getApi(leagueClicked);
+            break;
+            case "Copa Libertadores":
+            leagueClicked = "/competitions/CLI/"
+            getApi(leagueClicked);
+            break;
+        }
+    })}
+
+
+
+
+function descript(match, homeImg, awayImg){
+
+    console.log(match.status);
+    
+    let matchDate = match.utcDate.split("T")
+    let time = matchDate[1].replace("Z","");
+    console.log(matchDate);
+    console.log(time);
+    matchDate = matchDate[0].split("-");
+    matchDate = `${matchDate[1]}/${matchDate[2]}/${matchDate[0]}`
+    console.log(matchDate);
+    
+    // The idea for this if statement is that if a user searches a match, then we search for the match status so that we can see if the match either started, finished, or will happen at a certain time and date. It should display in the hover: what league the match is from, score if any, whatever else you can think of 
+    let hoverDescript = "";
+    if(match.status === "SCHEDULED"){
+        hoverDescript = `
+            <p> ${match.status}: ${matchDate} </p>
+        `
+    }
+    else if(match.status === "FINISHED" || match.status === "IN_PLAY"){
+        
+        hoverDescript = `
+        <p> ${match.score.fullTime.homeTeam} </p>
+        <p> ${match.score.fullTime.awayTeam} </p>
+        `
+    }
+    else if(match.status === "POSTPONED"){
+        hoverDescript= `<p> Date: TBD </p>`
+    }
+    
+
+    let displayBox = `
+      <div class="card">
+        <h2 class="card-title"><span>${match.homeTeam.name} (HOME)</span> VS <span>${match.awayTeam.name} (AWAY)</span></h2>
+        <div class="content">
+            <img src="${homeImg}" alt="">
+            
+            <div class="centerDescrip">
+            <p class="matchDate">${matchDate} </p>
+
+            <p class="matchTime">${time}</p>
+            
+            <p class=matchStatus">${match.status}</p>
+            </div>
+            
+            <img src="${awayImg}" alt="">
+        </div>
+        <div class="card-desc"> 
+            ${hoverDescript}
+        </div>
+      </div>`;
+      return displayBox;
+}
+
+async function getApi(id, startDate, endDate) {
+    console.log(startDate);
+    if(!id){
+        if(!startDate && !endDate){
+        let date = new Date();
+        console.log(date);
+        let year = date.getFullYear();
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        id = "/"
+        startDate = `${year}-0${month}-${day}`;
+        endDate = `${year}-0${month}-${day}`; // I had to input the 0 in order for this to work. An issue would happen if the month and day are single digits
+        }
+    }
+    
+    if(!startDate && !endDate){
+        let date = new Date();
+        console.log(date);
+        let year = date.getFullYear();
+        let day = date.getDate();
+        if(day.length === 1){
+            day = `0${day}`;
+        }
+        if(month.length === 1){
+            month = `0${month}`;
+        }
+        let month = date.getMonth() + 1;
+        startDate = `${year}-${month}-${day}`;
+        endDate = `${year}-${month}-${day}`; // I had to input the 0 in order for this to work. An issue would happen if the month and day are single digits
+        }
+
+        console.log(id);
+    console.log(id, startDate, endDate);
+    const main = document.querySelector("main");
+    const fetchApi = await fetch(`https://api.football-data.org/v2${id}matches?dateFrom=${startDate}&dateTo=${endDate}`, options)
+    const response = await fetchApi.json()
+    if(response.errorCode){
+        if(response.message.includes("10")){
+            return main.innerText = response.message;
+        }
+        else{
+            return main.innerText = "Argument 'Start Date' must be dated before the one specified in 'End Date'"
+        }
+    } 
+    const homeTeam = document.createElement("div")
+    homeTeam.setAttribute("class", "cards");
+    
+    main.innerHTML = "";
+    main.append(homeTeam);
+    
+
+    for(let i=0; i<=Math.min(4,response.matches.length-1);i++){
+        console.log(i,response.matches.length, response.matches);
+        let homeImg = await getTeamLogo(response.matches[i].homeTeam.id);
+        let awayImg = await getTeamLogo(response.matches[i].awayTeam.id);
+        
+        homeTeam.innerHTML += descript(response.matches[i], homeImg, awayImg);
+    }
+
+}
+
+
+async function getTeamLogo(id) {
+    console.log(id);
+    const fetchApi = await fetch(`http://api.football-data.org/v2/teams/${id}`, options) 
+    const response = await fetchApi.json()
+
+    return response.crestUrl;
+}
+
 
